ca59a4b5088a4d0fc097778ea936eac21edbd83f
zacharie.grastien <zacharie.grastien@TLPU066.ETAB.LOCAL>
2025-05-26 12:58:39 +0200

    cool

--- subscribe.py
+++ subscribe.py
@@ -21,46 +21,78 @@ backdrop = pygame.transform.scale(backdrop, (decorWidth, backdrop.get_height()/b
 player = {"x":0 , "y":0 , "z":0, "dir":0}
 cJumpSprite = pygame.image.load("images/player/chargedjump.png").convert_alpha()
 cJumping = False
-playerSprites = {"front":[pygame.image.load("images/player/forward.png").convert_alpha()],"left":[pygame.image.load("images/player/left.png").convert_alpha()],"right":[pygame.image.load("images/player/right.png").convert_alpha()],"charged":[cJumpSprite.subsurface(0,0,32,32),cJumpSprite.subsurface(32,0,32,32)]}
+cJumpingTimer = 0
+playerSprites = {"front":[pygame.image.load("images/player/forward.png").convert_alpha()],"left":[pygame.image.load("images/player/left.png").convert_alpha()],"right":[pygame.image.load("images/player/right.png").convert_alpha()],"charged":[cJumpSprite.subsurface(0,0,32,32),cJumpSprite.subsurface(32,0,32,32)], "driftingLeft":[pygame.image.load("images/player/left2.png").convert_alpha()], "driftingRight":[pygame.image.load("images/player/right2.png").convert_alpha()]}
 keysPressed = {}
-spd = 1.2
+spd = 0.6
 timer = 0
 animSpeed = 10
+drift = 0
+drifting = False
+driftingLeft = False
 
 def updatePlayer():
-    if keysPressed[pygame.K_LEFT]:
+    global cJumping, cJumpingTimer, drift, driftingLeft, drifting
+    if not keysPressed[pygame.K_SPACE]:
+        drifting = False
+    if not cJumping and (not drifting or (driftingLeft and keysPressed[pygame.K_SPACE])) and (keysPressed[pygame.K_LEFT] or (driftingLeft and drifting)):
         player["dir"] += spd
-    elif keysPressed[pygame.K_RIGHT]:
-        player["dir"] += -spd
+        if pygame.K_SPACE in newKeys:
+            drift = 0
+            drifting = True
+            driftingLeft = True
+        elif keysPressed[pygame.K_SPACE] and drifting:
+            drift += 1
+            player["dir"] += spd/2
+    elif not cJumping and (not drifting or (not driftingLeft and keysPressed[pygame.K_SPACE])) and (keysPressed[pygame.K_RIGHT] or (not driftingLeft and drifting)):
+        player["dir"] -= spd
+        if pygame.K_SPACE in newKeys:
+            drift = 0
+            drifting = True
+            driftingLeft = False
+        elif keysPressed[pygame.K_SPACE] and drifting:
+            drift += 1
+            player["dir"] -= spd/2
+    else:
+        if pygame.K_SPACE in newKeys:
+            if not cJumping:
+                cJumping = True
+                cJumpingTimer = 0
+        elif not keysPressed[pygame.K_SPACE]:
+            if cJumpingTimer > 60 and cJumping:
+                print('yoo')
+            cJumping = False
+        elif keysPressed[pygame.K_SPACE]:
+            cJumpingTimer += 1
 
 def drawPlayer():
     playerSprite = "front"
-    if keysPressed[pygame.K_LEFT]:
+    if cJumping:
+        playerSprite = "charged"
+    elif drifting:
+        if not driftingLeft:
+            playerSprite = "driftingLeft"
+        else:
+            playerSprite = "driftingRight"
+    elif keysPressed[pygame.K_LEFT]:
         playerSprite = "left"
     elif keysPressed[pygame.K_RIGHT]:
         playerSprite = "right"
-    elif keysPressed[pygame.K_SPACE]:
-        playerSprite = "charged"
     sprite = playerSprites[playerSprite][math.floor(timer/animSpeed)%len(playerSprites[playerSprite])]
     screen.blit(sprite, (screen.get_width()/2-sprite.get_width()/2, screen.get_height()/2-sprite.get_height()/2))
 
 while True:
     screen = pygame.surface.Surface(displaySize)
+    newKeys = []
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-    
-    newKeysPressed = {}
-    for key in newKeysPressed:
-        if key not in keysPressed:
-            keysPressed[key] = True
-        else:
-            keysPressed[key] = False
+            newKeys.append(event.key)
 
     keysPressed = pygame.key.get_pressed()
     timer += 1
